ffa03e78dbb68a7acea8e59532214fe237d19a9e
Joseph Huynh <joe.huynh@gmail.com>
2016-07-26 22:38:19 +0000

    Implemented user stories

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var upload = multer();
 
 var port = process.env.PORT || 8080;
 
+app.post('/get-file-size', upload.single('file'), function(req, res){
+    res.send({'size': req.file.size});
+});
+
 // Serve default page
-app.get('/', function(req, res){
+app.get('/*', function(req, res){
     res.sendFile('index.html', {root: __dirname});
 });
 
